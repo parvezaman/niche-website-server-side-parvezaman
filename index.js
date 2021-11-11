a6bff03d987cb53bf1717b224af9a46e26bac077
parvezaman <emailtoamanullah@gmail.com>
2021-11-12 00:35:16 +0600

    all products loading on server

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors =require('cors');
 const app = express();
 require('dotenv').config();
 
 //PORT
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 //MongoBD Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbuol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -17,13 +21,25 @@ async function run() {
       await client.connect();
       const database = client.db("shop-and-shoot");
       const haiku = database.collection("test");
-      // create a document to insert
-      const doc = {
-        title: "Record of a Shriveled Datum",
-        content: "No bytes, no problem. Just insert a document, in MongoDB",
-      }
-      const result = await haiku.insertOne(doc);
-      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      const productCollection = database.collection("productCollection");
+
+      // GET Products API
+      app.get('/products', async(req, res)=>{
+        const cursor = productCollection.find({});
+        const products = await cursor.toArray();
+        res.send(products);
+      })
+      // POST API
+      app.post('/products', async(req, res)=>{
+        const product = req.body;
+        const result = await productCollection.insertOne(product);
+        // console.log("hit the post");
+        res.json(result);
+      })
+
+
+      // const result = await haiku.insertOne(doc);
+      // console.log(`A document was inserted with the _id: ${result.insertedId}`);
     } finally {
     //   await client.close();
     }
